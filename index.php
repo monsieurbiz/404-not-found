830bcc6d937904e7dcfda546f03c0c6889341317
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2025-02-24 22:14:13 +0100

    feat: Add the 404 page with title and content vars

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+
+header('HTTP/1.1 404 Not Found');
+
+$title = isset($_GET['title']) ? htmlspecialchars($_GET['title'], ENT_QUOTES | ENT_SUBSTITUTE) : '404 Not Found';
+$content = isset($_GET['content']) ? htmlspecialchars($_GET['content'], ENT_QUOTES | ENT_SUBSTITUTE) : '';
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title><?php echo $title; ?></title>
+  <link rel="icon" href="/favicon.png">
+  <style>
+    @font-face{font-display:swap;font-family:Archivo;font-stretch:100%;font-style:normal;font-weight:100 900;src:url(/fonts/Archivo-VariableFont-Latin.woff2)format("woff2");unicode-range:U+??,U+131,U+152-153,U+2BB-2BC,U+2C6,U+2DA,U+2DC,U+304,U+308,U+329,U+2000-206F,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD}
+    @font-face{font-display:swap;font-family:Archivo;font-stretch:100%;font-style:normal;font-weight:100 900;src:url(/fonts/Archivo-VariableFont-LatinExt.woff2)format("woff2");unicode-range:U+100-2BA,U+2BD-2C5,U+2C7-2CC,U+2CE-2D7,U+2DD-2FF,U+304,U+308,U+329,U+1D00-1DBF,U+1E00-1E9F,U+1EF2-1EFF,U+2020,U+20A0-20AB,U+20AD-20C0,U+2113,U+2C60-2C7F,U+A720-A7FF}
+    @font-face{font-display:swap;font-family:Dela Gothic One;font-style:normal;font-weight:700;src:url(/fonts/DelaGothicOne-Regular.woff2)format("woff2")}
+    body {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      min-height: 100vh;
+      margin: 0;
+      font-family: Arial, sans-serif;
+      text-align: center;
+      background-color: rgb(7 53 61);
+    }
+    img {
+      max-width: 200px;
+      margin-bottom: 20px;
+    }
+    h1 {
+      font-size: 2.5rem;
+      margin-bottom: 10px;
+      color: rgb(255 239 234);
+      font-family: Dela Gothic One, sans-serif;
+      font-weight: 700;
+      letter-spacing: 2px;
+    }
+    h1:after {
+        color: rgb(253 136 111);
+        content: ".";
+    }
+    p {
+      font-size: 1.2rem;
+      color: rgb(255, 255, 255);
+      font-family: Archivo, sans-serif;
+      font-weight: 500;
+    }
+  </style>
+</head>
+<body>
+  <img src="https://monsieurbiz.com/media/resources/logo_white.png" alt="Monsieur Biz" title="Monsieur Biz">
+  <h1><?php echo $title; ?></h1>
+  <?php if (!empty($content)): ?>
+  <p><?php echo $content; ?></p>
+  <?php endif; ?>
+</body>
+</html>
